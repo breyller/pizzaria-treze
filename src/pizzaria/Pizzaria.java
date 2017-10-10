a2410f715b2e17e40770dbbc4b8b347b2c1b4cb7
breyller <brunoreyller@gmail.com>
2017-10-10 11:42:26 -0300

    gitignore e pasta src

---
+++ src/pizzaria/Pizzaria.java
@@ -0,0 +1,15 @@
+package pizzaria;
+
+/**
+ *
+ * @author Bruno Reyller
+ * @author Marina Mergulhão
+ * @author Gabriel Kiciolar
+ */
+public class Pizzaria {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
