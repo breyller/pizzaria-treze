39ef70a606cf705d444b2cd1c7529fbe48ba9907
breyller <brunoreyller@gmail.com>
2017-10-17 02:36:45 -0200

    tabela clientes

--- src/BD/pizzaria.sql
+++ src/BD/pizzaria.sql
@@ -2,3 +2,15 @@ CREATE DATABASE IF NOT EXISTS pizzaria;
 
 USE pizzaria;
 
+DROP TABLE `cliente`;
+
+CREATE IF NOT EXISTS TABLE `cliente` ( 
+	`id` Int( 11 ) UNSIGNED NOT NULL,
+	`nome` VarChar( 127 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+	`endereco` VarChar( 255 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+	`telefone` VarChar( 15 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+	`cpf` VarChar( 15 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+	PRIMARY KEY ( `id` ) )
+CHARACTER SET = latin1
+COLLATE = latin1_swedish_ci
+ENGINE = InnoDB;
