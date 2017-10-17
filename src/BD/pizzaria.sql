61bcdb474f2ebf2f4b378cd7eaffd74839af3236
breyller <brunoreyller@gmail.com>
2017-10-17 10:14:55 -0200

    tabela reserva

--- src/BD/pizzaria.sql
+++ src/BD/pizzaria.sql
@@ -4,13 +4,13 @@ USE pizzaria;
 
 DROP TABLE IF EXISTS `cliente` CASCADE;
 
-CREATE IF NOT EXISTS TABLE `cliente` ( 
+CREATE IF NOT EXISTS TABLE `cliente` (
 	`id` INT(11) UNSIGNED NOT NULL,
 	`nome` VARCHAR(127) NOT NULL,
 	`endereco` VARCHAR(255) NOT NULL,
 	`telefone` VARCHAR(15) NOT NULL,
 	`cpf` VARCHAR(15) NOT NULL,
-	PRIMARY KEY ( `id` ) )
+	PRIMARY KEY (`id`))
 CHARACTER SET = latin1
 COLLATE = latin1_swedish_ci
 ENGINE = InnoDB;
@@ -18,21 +18,21 @@ ENGINE = InnoDB;
 
 DROP TABLE IF EXISTS `funcionario` CASCADE;
 
-CREATE TABLE `funcionario` ( 
+CREATE TABLE `funcionario` (
 	`id` INT(11) UNSIGNED NOT NULL,
-	`nome` VARCHAR( 127 ) NOT NULL,
-	`endereco` VARCHAR( 255 ) NOT NULL,
-	`telefone` VARCHAR( 15 ) NOT NULL,
-	`cpf` VARCHAR( 15 ) NOT NULL,
-	`cargo` VARCHAR( 31 ) NOT NULL,
-	PRIMARY KEY ( `id` ) )
+	`nome` VARCHAR(127) NOT NULL,
+	`endereco` VARCHAR(255) NOT NULL,
+	`telefone` VARCHAR(15) NOT NULL,
+	`cpf` VARCHAR(15) NOT NULL,
+	`cargo` VARCHAR(31) NOT NULL,
+	PRIMARY KEY (`id`))
 CHARACTER SET = latin1
 COLLATE = latin1_swedish_ci
 ENGINE = InnoDB;
 
 DROP TABLE IF EXISTS `mesa` CASCADE;
 
-CREATE TABLE `mesa` ( 
+CREATE TABLE `mesa` (
 	`id` INT(11) UNSIGNED NOT NULL,
 	`numero` SMALLINT(5) UNSIGNED NOT NULL,
 	`qtd_lugares` TINYINT(3) UNSIGNED NOT NULL,
@@ -41,3 +41,14 @@ CHARACTER SET = latin1
 COLLATE = latin1_swedish_ci
 ENGINE = InnoDB;
 
+DROP TABLE IF EXISTS `reserva` CASCADE;
+
+CREATE TABLE `reserva` (
+	`id` INT(11) UNSIGNED NOT NULL,
+	`data` DATETIME NOT NULL,
+	`status` TINYINT(3) NOT NULL,
+	`composicao` VARCHAR(511) NOT NULL,
+	PRIMARY KEY (`id`))
+CHARACTER SET = latin1
+COLLATE = latin1_swedish_ci
+ENGINE = InnoDB;
