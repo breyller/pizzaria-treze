d67b4c80656cb2406c0cf89366c2d935074fcc37
breyller <brunoreyller@gmail.com>
2017-11-18 11:54:30 -0200

    ajuste no model de pedido

--- src/model/Pedido.java
+++ src/model/Pedido.java
@@ -1,13 +1,19 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class Pedido {
     private int id;
+    private String data;
+    private int status;
     private String descricao;
-    private float valorTotal;
-    private String status;
+    private int tipo;
+    private double preco;
     private int idCliente;
-    private int[] idPrdutos;
+    private int idFuncionario;
+    private ArrayList<Integer> composicao;
 
     public int getId() {
         return id;
@@ -15,6 +21,22 @@ public class Pedido {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
     }
 
     public String getDescricao() {
@@ -25,20 +47,20 @@ public class Pedido {
         this.descricao = descricao;
     }
 
-    public float getValorTotal() {
-        return valorTotal;
+    public int getTipo() {
+        return tipo;
     }
 
-    public void setValorTotal(float valorTotal) {
-        this.valorTotal = valorTotal;
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
     }
 
-    public String getStatus() {
-        return status;
+    public double getPreco() {
+        return preco;
     }
 
-    public void setStatus(String status) {
-        this.status = status;
+    public void setPreco(double preco) {
+        this.preco = preco;
     }
 
     public int getIdCliente() {
@@ -49,14 +71,30 @@ public class Pedido {
         this.idCliente = idCliente;
     }
 
-    public int[] getIdPrdutos() {
-        return idPrdutos;
+    public int getIdFuncionario() {
+        return idFuncionario;
     }
 
-    public void setIdPrdutos(int[] idPrdutos) {
-        this.idPrdutos = idPrdutos;
+    public void setIdFuncionario(int idFuncionario) {
+        this.idFuncionario = idFuncionario;
     }
-    
+
+    public ArrayList<Integer> getComposicao() {
+        return composicao;
+    }
+
+    public void setComposicao(int idProduto) {
+        this.composicao.add(idProduto);
+    }
+
+    public void deleteComposicaoByPos(int posicao) {
+        this.composicao.remove(posicao);
+    }
+
+    public void deleteComposicaoByValor(int valor) {
+        int pos = this.composicao.lastIndexOf(valor);
+        deleteComposicaoByPos(pos);
+    }
     
     
 }
