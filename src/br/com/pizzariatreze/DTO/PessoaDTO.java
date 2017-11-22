3990172dd921a93dc9bf59b1e740c330b54a8587
breyller <brunoreyller@gmail.com>
2017-11-22 21:49:06 -0200

    pasta DTO

---
+++ src/br/com/pizzariatreze/DTO/PessoaDTO.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.pizzariatreze.DTO;
+
+/**
+ *
+ * @author Fabio
+ */
+public class PessoaDTO {
+    private int id;
+    private String nome;
+    private String endereco;
+    private String telefone;
+    private String cpf;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
