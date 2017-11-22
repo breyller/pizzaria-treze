88c21f9f292445cfda44e63b44534cccc21c5e07
breyller <brunoreyller@gmail.com>
2017-11-22 20:09:55 -0200

    ajuste nome packages

---
+++ src/br/com/pizzariatreze/model/Cliente.java
@@ -0,0 +1,31 @@
+package br.com.pizzariatreze.model;
+
+import br.com.pizzariatreze.DAO.ClienteDAO;
+import java.util.ArrayList;
+
+public class Cliente extends Pessoa {
+    
+    public ArrayList<Cliente> getByNome(String nome) {
+        ArrayList<Cliente> result = null;
+        ClienteDAO clienteDao = new ClienteDAO();
+        
+        result = clienteDao.getByNome(nome);
+        return result;
+    }
+
+    public Cliente getById(int id) {
+        Cliente result = null;
+        ClienteDAO clienteDao = new ClienteDAO();
+        
+        result = clienteDao.getById(id);
+        return result;
+    }
+    
+    public String save() {
+        String result = null;
+        ClienteDAO clienteDao = new ClienteDAO();
+        
+        result = clienteDao.save(this);
+        return result;
+    }    
+}
---
+++ src/br/com/pizzariatreze/model/Gerente.java
@@ -0,0 +1,10 @@
+package br.com.pizzariatreze.model;
+
+public class Gerente extends Funcionario {
+    private String cargo;
+    
+    public Gerente() {
+        this.cargo = "Gerente";
+    }    
+    
+}
