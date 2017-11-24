c1bfceef4e8218ae50e34920369850057848a9f0
breyller <brunoreyller@gmail.com>
2017-11-24 00:14:16 -0200

    exemplo de controller de login

--- src/br/com/pizzariatreze/Controller/LoginController.java
+++ src/br/com/pizzariatreze/Controller/LoginController.java
@@ -1,19 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.pizzariatreze.Controller;
 
 import br.com.pizzariatreze.model.Funcionario;
 
-/**
- *
- * @author Fabio
- */
 public class LoginController {
     
-    public LoginController(){}
+    public LoginController(){
+    }
     
     public boolean logar(String usuario, String senha){
         Funcionario funcionario = new Funcionario();
