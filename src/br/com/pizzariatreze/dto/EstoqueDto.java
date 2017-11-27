0bf5fe0910c7ea2c4784d3b9e63356bf07a948ed
breyller <brunoreyller@gmail.com>
2017-11-27 13:53:37 -0200

    estoquedto

---
+++ src/br/com/pizzariatreze/dto/EstoqueDto.java
@@ -0,0 +1,9 @@
+package br.com.pizzariatreze.dto;
+
+public class EstoqueDto {
+
+    public Object getId() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
