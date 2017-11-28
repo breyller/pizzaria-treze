318031077db8ef44144da9f3f63ab6e7214cdfc4
breyller <brunoreyller@gmail.com>
2017-11-28 01:30:15 -0200

    alterado no ingrediente

--- src/br/com/pizzariatreze/dto/IngredienteDto.java
+++ src/br/com/pizzariatreze/dto/IngredienteDto.java
@@ -1,5 +1,8 @@
 package br.com.pizzariatreze.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IngredienteDto {
 
     private int id;
@@ -7,6 +10,11 @@ public class IngredienteDto {
     private String descricao;
     private int quantidade;
     private double valor;
+    protected List alterado;
+    
+    public IngredienteDto(){
+        this.alterado = new ArrayList<>();
+    }
 
     public int getId() {
         return id;
@@ -14,6 +22,8 @@ public class IngredienteDto {
 
     public void setId(int id) {
         this.id = id;
+        
+        if(!this.alterado.contains("id")) this.alterado.add("id");
     }
 
     public String getNome() {
@@ -22,6 +32,8 @@ public class IngredienteDto {
 
     public void setNome(String nome) {
         this.nome = nome;
+        
+        if(!this.alterado.contains("nome")) this.alterado.add("nome");
     }
 
     public int getQuantidade() {
@@ -30,6 +42,8 @@ public class IngredienteDto {
 
     public void setQuantidade(int quantidade) {
         this.quantidade = quantidade;
+        
+        if(!this.alterado.contains("quantidade")) this.alterado.add("quantidade");
     }
 
     public double getValor() {
@@ -38,6 +52,8 @@ public class IngredienteDto {
 
     public void setValor(double valor) {
         this.valor = valor;
+        
+        if(!this.alterado.contains("valor")) this.alterado.add("valor");
     }
     
     public String getDescricao() {
@@ -46,5 +62,11 @@ public class IngredienteDto {
 
     public void setDescricao(String descricao) {
         this.descricao = descricao;
+        
+        if(!this.alterado.contains("descricao")) this.alterado.add("descricao");
+    }
+
+    public List getAlterado() {
+        return this.alterado;
     }
 }
