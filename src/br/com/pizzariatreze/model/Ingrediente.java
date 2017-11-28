8fe6866369a8b29717f77f2f8d648cd81e44f720
breyller <brunoreyller@gmail.com>
2017-11-28 01:35:48 -0200

    finalizando models

--- src/br/com/pizzariatreze/model/Ingrediente.java
+++ src/br/com/pizzariatreze/model/Ingrediente.java
@@ -2,6 +2,7 @@ package br.com.pizzariatreze.model;
 
 import br.com.pizzariatreze.dao.IngredienteDao;
 import br.com.pizzariatreze.dto.IngredienteDto;
+import java.util.List;
 import java.util.Map;
 
 public class Ingrediente {
@@ -13,9 +14,22 @@ public class Ingrediente {
         if(ingrediente.containsKey("id")) ingredienteDto.setId((int)ingrediente.get("id"));
         if(ingrediente.containsKey("nome")) ingredienteDto.setNome((String)ingrediente.get("nome"));
         if(ingrediente.containsKey("descricao")) ingredienteDto.setDescricao((String)ingrediente.get("descricao"));
-        if(ingrediente.containsKey("valor")) ingredienteDto.setValor((double)ingrediente.get("valor"));
+        if(ingrediente.containsKey("valor")) ingredienteDto.setValor(Double.parseDouble((String)ingrediente.get("valor")));
+        if(ingrediente.containsKey("quantidade")) ingredienteDto.setQuantidade(Integer.parseInt((String)ingrediente.get("quantidade")));
         
         return ingredienteDao.save(ingredienteDto);
     } 
+    
+        public List<Object> listar() {
+        /* Criação do modelo */
+            IngredienteDto ingrediente = new IngredienteDto();
+ 
+            /* Criação do DAO */
+            IngredienteDao ddao = new IngredienteDao();
+            List<Object> lista;
+            lista = ddao.search(ingrediente);
+            
+            return lista;    
+    }
     
 }
