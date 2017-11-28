7e82ab6fa5a1285442f1c4c3a44d61518fe8366b
breyller <brunoreyller@gmail.com>
2017-11-28 01:19:08 -0200

    listar ingredienteController

--- src/br/com/pizzariatreze/controller/IngredienteController.java
+++ src/br/com/pizzariatreze/controller/IngredienteController.java
@@ -1,6 +1,7 @@
 package br.com.pizzariatreze.controller;
 
 import br.com.pizzariatreze.model.Ingrediente;
+import java.util.List;
 import java.util.Map;
 
 public class IngredienteController {
@@ -9,5 +10,10 @@ public class IngredienteController {
         Ingrediente ingrediente = new Ingrediente();
         return ingrediente.save(mapIngrediente);
     }
+
+     public List<Object> listar() {
+        Ingrediente ingrediente = new Ingrediente();
+        return ingrediente.listar();
+    } 
     
 }
