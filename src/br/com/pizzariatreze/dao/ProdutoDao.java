8070295e36b827d1089ed97b1393c14c21307d29
breyller <brunoreyller@gmail.com>
2017-12-09 14:52:03 -0200

    ajustes no dao de produto

--- src/br/com/pizzariatreze/dao/ProdutoDao.java
+++ src/br/com/pizzariatreze/dao/ProdutoDao.java
@@ -7,12 +7,17 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ProdutoDao {
 
-    private ArrayList<ProdutoDto> produtos = null;
+    private ArrayList<ProdutoDto> produtos;
     private ProdutoDto produto = null;
     private Connection con = null;
+
+    public ProdutoDao() {
+        this.produtos = new ArrayList<>();
+    }
     
     public ProdutoDto getById(int id) {
         this.produto = null;
@@ -34,6 +39,7 @@ public class ProdutoDao {
                 this.produto.setDescricao(rs.getString("descricao"));
                 ingredientes = rs.getString("composicao");
                 ingredientesSplit = ingredientes.split(",");
+                
                 for (int i = 0; i < ingredientesSplit.length; i++) {
                     this.produto.setComposicao(ingredienteDao.getById(Integer.parseInt(ingredientesSplit[i])));
                 }
@@ -206,42 +212,51 @@ public class ProdutoDao {
                     ps = Conexao.getConexao().prepareStatement(query);
                     ps.setString(1, produto.getNome());
                     ps.setDouble(2, produto.getPreco());
-                    ingredientes = String.valueOf(produto.getComposicao().get(0).getId());
-                    for(int i = 1; i < produto.getComposicao().size(); i++) {
-                        ingredientes = ingredientes + "," + String.valueOf(produto.getComposicao().get(i).getId());
+                    ps.setString(3, produto.getDescricao());
+                    ingredientes = "";
+                    if(produto.getComposicao() != null) {
+                        ingredientes = String.valueOf(produto.getComposicao().get(0).getId());
+                        for(int i = 1; i < produto.getComposicao().size(); i++) {
+                            ingredientes = ingredientes + "," + String.valueOf(produto.getComposicao().get(i).getId());
+                        }
+                    }else if(!produto.getComposicaoString().equals("")){
+                        ingredientes = produto.getComposicaoString();
                     }
-                    ps.setString(3, ingredientes);
-                    ps.setInt(4, produto.getId());
+                    ps.setString(4, ingredientes);
+                    ps.setInt(5, produto.getId());
                     ps.executeUpdate();
                     
                     return true;
                 } catch (SQLException ex) {
-                    //criar log
-                    //"Erro ao atualizar produto: " + ex.getMessage();
+                    ex.printStackTrace();
                     return false;
                 }
             }
         }
         
         try {
-            query = produto.getId() != 0 ? "INSERT INTO produto(nome, preco, descricao, composicao, id) VALUES (?,?,?,?,?)" : "INSERT INTO cliente(nome, preco, descricao, composicao) VALUES (?,?,?,?)";
+            query = "INSERT INTO produto(nome, preco, descricao, composicao) VALUES (?,?,?,?)";
             ps = Conexao.getConexao().prepareStatement(query);
             ps.setString(1, produto.getNome());
             ps.setDouble(2, produto.getPreco());
             ps.setString(3, produto.getDescricao());
-            ingredientes = String.valueOf(produto.getComposicao().get(0).getId());
-            for(int i = 1; i < produto.getComposicao().size(); i++) {
-                ingredientes = ingredientes + "," + String.valueOf(produto.getComposicao().get(i).getId());
+            
+            ingredientes = "";
+            if(produto.getComposicao().size() > 0) {
+                ingredientes = String.valueOf(produto.getComposicao().get(0).getId());
+                for(int i = 1; i < produto.getComposicao().size(); i++) {
+                    ingredientes = ingredientes + "," + String.valueOf(produto.getComposicao().get(i).getId());
+                }
+            }else if(!produto.getComposicaoString().equals("")){
+                ingredientes = produto.getComposicaoString();
             }
+            
             ps.setString(4, ingredientes);
-            if(produto.getId() != 0) {
-                ps.setInt(5, produto.getId());
-            }
+            
             ps.executeUpdate();
             return true;
         } catch (SQLException ex) {
-            //criar log
-            //"Erro ao inserir produto: " + ex.getMessage();
+            ex.printStackTrace();
             return false;
         }
     }
@@ -262,5 +277,110 @@ public class ProdutoDao {
             //"Erro ao excluir produto: " + ex.getMessage();
             return false;
         }
+    }
+
+    public Integer pesquisarPizza(String composicao) {
+        //this.produto = null;
+        //this.produtos.clear();
+        PreparedStatement ps = null;
+        String ingredientes = null;
+        //String[] ingredientesSplit = null;
+        IngredienteDao ingredienteDao = new IngredienteDao();
+        
+        try {
+            con = Conexao.getConexao();
+            ps = con.prepareStatement("SELECT id FROM produto WHERE composicao = ?");
+            ps.setString(1, composicao);
+            ResultSet rs = ps.executeQuery();
+            
+             if(!rs.next()) {
+                return 0;
+            }
+             
+            return rs.getInt("id");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    
+    public List<Object> search(ProdutoDto produto) {
+ 
+        List<Object> produtosObj = new ArrayList<>();
+        
+        try{
+            con = Conexao.getConexao();
+            String sql = "SELECT * FROM produto";
+            PreparedStatement ps = null;
+        
+            List parametros = new ArrayList<>();
+
+            if(produto.getId() > 0){
+                sql += " WHERE ID = ? ";
+                parametros.add(produto.getId());
+            }else{
+                List alt = produto.getAlterado();
+                String sqlWhere = "";
+                int index = 1;
+
+                if(alt.contains("nome")) {
+                    sqlWhere += " AND nome = ? ";
+                    parametros.add(produto.getNome());
+                }
+
+                if(alt.contains("descricao")){
+                    sqlWhere += " AND descricao = ? ";
+                    parametros.add(produto.getDescricao());
+                }
+
+                if(alt.contains("preco")){
+                    sqlWhere += " AND preco = ? ";
+                    parametros.add(produto.getPreco());
+                }
+
+                if(alt.contains("composicao")){
+                    sqlWhere += " AND composicao = ? ";
+                    parametros.add(produto.getComposicaoString());
+                }
+
+                if(sqlWhere.length() > 0) sql += " WHERE 1=1 " + sqlWhere;
+            }
+
+            ps = con.prepareStatement(sql);
+            int index = 1;
+
+            for(Object i : parametros){
+                ps.setObject(index, i);
+                index++;
+            }
+
+            ResultSet rs = ps.executeQuery();
+
+            if(rs.next()) {
+                do {
+                    this.produto = new ProdutoDto();
+                    this.produto.setId(rs.getInt("id"));
+                    this.produto.setNome(rs.getString("nome"));
+                    this.produto.setDescricao(rs.getString("descricao"));
+                    this.produto.setPreco(rs.getInt("preco"));
+                    this.produto.setComposicao(rs.getString("composicao"));
+                    this.produtos.add(this.produto);
+                    produtosObj.add((Object)this.produto);
+                } while (rs.next());
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        
+        return produtosObj;
     }
 }
