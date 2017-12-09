d5540dcc2e5a5307dd431d55fd9a16ba45cdcf24
breyller <brunoreyller@gmail.com>
2017-12-09 14:36:51 -0200

    listar e delete de produto

--- src/br/com/pizzariatreze/controller/ProdutoController.java
+++ src/br/com/pizzariatreze/controller/ProdutoController.java
@@ -1,13 +1,30 @@
 package br.com.pizzariatreze.controller;
 
 import br.com.pizzariatreze.model.Produto;
+import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class ProdutoController {
 
-        public boolean save(Map mapProduto) {
+    public boolean save(Map mapProduto) throws Exception {
         Produto produto = new Produto();
         return produto.save(mapProduto);
+    }
+    
+    public Object savePizza(String nome, String composicao){
+        Produto produto = new Produto();
+        return produto.savePizza(nome, composicao);
+    }
+
+    public List listar() {
+        Produto produto = new Produto();
+        return produto.listar();
+    }
+
+    public List listarProdutos() {
+        Produto produto = new Produto();
+        return produto.listarProdutos();
     }
     
 }
