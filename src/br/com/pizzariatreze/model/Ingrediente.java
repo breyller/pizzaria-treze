9d47b5845414e4cf3cbcede99edfb1eac2011f81
breyller <brunoreyller@gmail.com>
2017-12-10 10:04:07 -0200

    delete, listar e validacoes no save de ingrediente

--- src/br/com/pizzariatreze/model/Ingrediente.java
+++ src/br/com/pizzariatreze/model/Ingrediente.java
@@ -7,20 +7,68 @@ import java.util.Map;
 
 public class Ingrediente {
 
-    public boolean save(Map ingrediente) {
+    public boolean save(Map ingrediente) throws Exception{
         IngredienteDao ingredienteDao = new IngredienteDao();
         IngredienteDto ingredienteDto = new IngredienteDto();
 
-        if(ingrediente.containsKey("id")) ingredienteDto.setId((int)ingrediente.get("id"));
-        if(ingrediente.containsKey("nome")) ingredienteDto.setNome((String)ingrediente.get("nome"));
-        if(ingrediente.containsKey("descricao")) ingredienteDto.setDescricao((String)ingrediente.get("descricao"));
-        if(ingrediente.containsKey("valor")) ingredienteDto.setValor(Double.parseDouble((String)ingrediente.get("valor")));
-        if(ingrediente.containsKey("quantidade")) ingredienteDto.setQuantidade(Integer.parseInt((String)ingrediente.get("quantidade")));
+        if(ingrediente.containsKey("id")) {
+            ingredienteDto.setId((int)ingrediente.get("id"));
+        }
+        
+        if(ingrediente.containsKey("nome")) {
+            if (ingrediente.get("nome").toString().trim().isEmpty()) {
+                throw new Exception("Nome do ingrediente deve estar preenchido.");
+            }
+            ingredienteDto.setNome(ingrediente.get("nome").toString().trim());
+        }
+        
+        if(ingrediente.containsKey("descricao")) {
+            if (ingrediente.get("descricao").toString().trim().isEmpty()) {
+                throw new Exception("Descricao deve estar preenchida.");
+            }
+            ingredienteDto.setDescricao(ingrediente.get("descricao").toString().trim());
+        }
+        
+        if(ingrediente.containsKey("valor")) {
+            if (ingrediente.get("valor").toString().trim().isEmpty()) {
+                throw new Exception("Valor deve estar preenchido.");
+            }
+            
+            double valor;
+            
+            try {
+                valor = Double.parseDouble(ingrediente.get("valor").toString().trim());
+            } catch (Exception e) {
+                throw new Exception("Valor deve ser numérico.");
+            }
+                        
+            ingredienteDto.setValor(valor);
+        }
+        
+        if(ingrediente.containsKey("quantidade")) {
+            if (ingrediente.get("quantidade").toString().trim().isEmpty()) {
+                throw new Exception("Quantidade deve estar preenchido.");
+            }
+            
+            int qtd;
+            
+            try {
+                qtd = Integer.parseInt(ingrediente.get("quantidade").toString().trim());
+            } catch (Exception e) {
+                throw new Exception("Valor deve ser numérico.");
+            }
+            
+            if(qtd <= 0) {
+                throw new Exception("Quantidade deve ser um numero positivo.");
+            }
+            
+            ingredienteDto.setQuantidade(qtd);
+        }
         
         return ingredienteDao.save(ingredienteDto);
     } 
     
-        public List<Object> listar() {
+    public List<Object> listar() {
         /* Criação do modelo */
             IngredienteDto ingrediente = new IngredienteDto();
  
@@ -32,4 +80,25 @@ public class Ingrediente {
             return lista;    
     }
     
+        public List<Object> listar(String id) {
+        /* Criação do modelo */
+            IngredienteDto ingrediente = new IngredienteDto();
+            
+            if(id.length() > 0)
+                ingrediente.setId(Integer.parseInt(id));
+
+            /* Criação do DAO */
+            IngredienteDao ddao = new IngredienteDao();
+            List<Object> lista;
+            lista = ddao.search(ingrediente);
+            
+            return lista;
+            
+    }
+
+    public boolean delete(int id) {
+        IngredienteDao idao = new IngredienteDao();
+        return idao.delete(id);
+    }
+
 }
