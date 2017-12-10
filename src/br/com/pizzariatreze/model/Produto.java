defe9f7b005bf053a81e807b416d76187b3fd06c
breyller <brunoreyller@gmail.com>
2017-12-10 10:14:34 -0200

    validacoes no save, listar e listar produtos no model de produtos

--- src/br/com/pizzariatreze/model/Produto.java
+++ src/br/com/pizzariatreze/model/Produto.java
@@ -2,20 +2,83 @@ package br.com.pizzariatreze.model;
 
 import br.com.pizzariatreze.dao.ProdutoDao;
 import br.com.pizzariatreze.dto.ProdutoDto;
+import java.util.List;
 import java.util.Map;
 
 public class Produto {
 
-    public boolean save(Map produto) {
+    public boolean save(Map produto) throws Exception {
         ProdutoDao produtoDao = new ProdutoDao();
         ProdutoDto produtoDto = new ProdutoDto();
 
-        if(produto.containsKey("id")) produtoDto.setId((int)produto.get("id"));
-        if(produto.containsKey("nome")) produtoDto.setNome((String)produto.get("nome"));
-        if(produto.containsKey("descricao")) produtoDto.setDescricao((String)produto.get("descricao"));
-        if(produto.containsKey("preco")) produtoDto.setPreco(Double.parseDouble((String)produto.get("preco")));
- 
+        if(produto.containsKey("id")) {
+            produtoDto.setId((int)produto.get("id"));
+        }
+        
+        if(produto.containsKey("nome")) {
+            if (produto.get("nome").toString().trim().isEmpty()) {
+                throw new Exception("Nome deve estar preenchido.");
+            }
+            produtoDto.setNome(produto.get("nome").toString().trim());
+        }
+        
+        if(produto.containsKey("descricao")) {
+            if (produto.get("descricao").toString().trim().isEmpty()) {
+                throw new Exception("Descricao deve estar preenchido.");
+            }
+            produtoDto.setDescricao(produto.get("descricao").toString().trim());
+        }
+        
+        if(produto.containsKey("preco")) {
+            if (produto.get("preco").toString().trim().isEmpty()) {
+                throw new Exception("Preco deve estar preenchido.");
+            }
+            
+            double preco;
+
+            try {
+                preco = Double.parseDouble(produto.get("preco").toString().trim());
+            } catch (Exception e) {
+                throw new Exception("Preco deve ser um valor numérico.");
+            }
+            
+            produtoDto.setPreco(preco);
+        }
+        
         return produtoDao.save(produtoDto);
     }  
+
+    public Object savePizza(String nome, String composicao) {
+        ProdutoDao produtoDao = new ProdutoDao();
+        ProdutoDto produtoDto = new ProdutoDto();
+        
+        Integer id = produtoDao.pesquisarPizza(composicao);
+        
+        if(id == 0){
+            produtoDto.setNome(nome);
+            produtoDto.setComposicao(composicao);
+            
+            if(produtoDao.save(produtoDto))
+                savePizza(nome, composicao);
+        }
+        
+        return produtoDao.getById(id);
+    }
+
+    public List listar() {
+        ProdutoDao produtoDao = new ProdutoDao();
+        ProdutoDto produtoDto = new ProdutoDto();
+        
+        return produtoDao.search(produtoDto);
+    }
+
+    public List listarProdutos() {
+        ProdutoDao produtoDao = new ProdutoDao();
+        ProdutoDto produtoDto = new ProdutoDto();
+        
+        produtoDto.setComposicao("");
+        
+        return produtoDao.search(produtoDto);
+    }
 
 }
